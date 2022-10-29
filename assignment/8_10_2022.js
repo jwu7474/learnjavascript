ba09e86ee55fa91aaf4f51d0a167e4cd56ae57a5
Jason Wu <jwu7474@outlook.com>
2022-10-29 11:25:12 +0100

    working

--- assignment/8_10_2022.js
+++ assignment/8_10_2022.js
@@ -20,48 +20,48 @@
 // };
 // console.log(rank(9, 2)); //[2,9]
 
-const rank3 = (a, b, c) => {
-  if (a > b && a > c && b > c) {
-    return [c, b, a];
-  } else {
-    if (b > a && b < c && a > c) {
-      return [b, a, c];
-    } else {
-      if (c > a && c > b && a < b) {
-        return [a, b, c];
-      }
-    }
-    {
-      if (a > c && b > c && b > a) {
-        return [c, a, b];
-      }
-    }
-    if (c > a && c > b && b < a) {
-      return [b, a, c];
-    }
-    if (b < a && b < c && c < a) {
-      return [b, c, a];
-    }
-    if (b > c && b > a && c > a) {
-      return [a, c, b];
-    }
-  }
-};
-console.log(rank3(9, 5, 2)); //[2,5,9]
-console.log(rank3(9, 2, 5)); //[2,5,9]
-console.log(rank3(5, 9, 2)); //[2,5,9]
-console.log(rank3(5, 2, 9)); //[2,5,9]
-console.log(rank3(2, 9, 5)); //[2,5,9]
-console.log(rank3(2, 5, 9)); //[2,5,9]
-
-// const firstLetter = (arrOfStr) => {
-//   let sum = 0;
-//   for (i = 0; i < arrOfStr.length; i++) {
-//     sum = sum + arrOfStr[i].charAt(0);
+// const rank3 = (a, b, c) => {
+//   if (a > b && a > c && b > c) {
+//     return [c, b, a];
+//   } else {
+//     if (b > a && b < c && a > c) {
+//       return [b, a, c];
+//     } else {
+//       if (c > a && c > b && a < b) {
+//         return [a, b, c];
+//       }
+//     }
+//     {
+//       if (a > c && b > c && b > a) {
+//         return [c, a, b];
+//       }
+//     }
+//     if (c > a && c > b && b < a) {
+//       return [b, a, c];
+//     }
+//     if (b < a && b < c && c < a) {
+//       return [b, c, a];
+//     }
+//     if (b > c && b > a && c > a) {
+//       return [a, c, b];
+//     }
 //   }
-//   return sum;
 // };
-// console.log(firstLetter(["asd", "sdx", "wed"])); //asd
+// console.log(rank3(9, 5, 2)); //[2,5,9]
+// console.log(rank3(9, 2, 5)); //[2,5,9]
+// console.log(rank3(5, 9, 2)); //[2,5,9]
+// console.log(rank3(5, 2, 9)); //[2,5,9]
+// console.log(rank3(2, 9, 5)); //[2,5,9]
+// console.log(rank3(2, 5, 9)); //[2,5,9]
+
+const firstLetter = (arrOfStr) => {
+  let sum = 0;
+  for (i = 0; i < arrOfStr.length; i++) {
+    sum = sum + arrOfStr[i].charAt(0);
+  }
+  return sum;
+};
+console.log(firstLetter(["asd", "sdx", "wed"])); //asd
 
 // const eachOne = (str) => {
 //   var splitString = str.split("");

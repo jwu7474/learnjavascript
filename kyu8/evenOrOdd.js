79d69a367e212540e04b6924d4967a43db533358
Jason Wu <jwu7474@outlook.com>
2022-11-05 12:50:01 +0000

    refactor simplify

--- kyu8/evenOrOdd.js
+++ kyu8/evenOrOdd.js
@@ -1,9 +1,10 @@
 const even_or_odd = (num) => {
-  if (num % 2 === 0) {
-    return "even";
-  } else {
-    return "odd";
-  }
+  // if (num % 2 === 0) {
+  //   return "even";
+  // } else {
+  //   return "odd";
+  // }
+  return num % 2 === 0 ? "even" : "odd";
 };
 console.log(even_or_odd(4)); //even
 console.log(even_or_odd(1)); //odd

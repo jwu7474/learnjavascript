67ab01f6297e4a1925ca31f1720a260e1f3a671e
Jason Wu <jwu7474@outlook.com>
2022-11-05 12:40:54 +0000

    test

---
+++ kyu8/evenOrOdd.js
@@ -0,0 +1,9 @@
+const even_or_odd = (num) => {
+  if (num % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+};
+console.log(even_or_odd(4)); //even
+console.log(even_or_odd(1)); //odd
